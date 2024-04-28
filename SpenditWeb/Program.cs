9bb6549deb0f661995e2c3d0ff13fdfd47d7faee
whiteflags26 <whiteflags26@gmail.com>
2024-04-29 01:40:52 +0600

    Moved the dashboard functionality from Home to Dashboard controller

--- SpenditWeb/Program.cs
+++ SpenditWeb/Program.cs
@@ -35,7 +35,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
