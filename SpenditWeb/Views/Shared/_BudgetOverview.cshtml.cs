5bc6d45a8e61fad3b8a848c8ba5cbed3cd0e0bab
Ahmed Sadman Labib <aslabib44@gmail.com>
2024-06-01 00:43:48 +0600

    added partial view budget overview

---
+++ SpenditWeb/Views/Shared/_BudgetOverview.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SpenditWeb.Views.Shared
+{
+    public class _BudgetOverviewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
